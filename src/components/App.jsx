ed61f6311f8c6ae9df757984e916e5efc98cc397
jgestradam <jgestradam@unal.edu.co>
2021-06-24 11:19:23 -0500

    google trouble

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../styles/components/App.css';
+
+const App = () => (
+  <div>
+    <h1>Hola Mundo</h1>
+  </div>
+);
+
+export default App;
---
+++ src/containers/Payment.jsx
@@ -0,0 +1,73 @@
+import React, { useContext } from 'react';
+import { PayPalButton } from 'react-paypal-button-v2';
+import AppContext from '../context/AppContext';
+import '../styles/components/Payment.css';
+
+const Payment = ({ history }) => {
+  const { state, addNewOrder } = useContext(AppContext);
+  const { cart, buyer } = state;
+
+  const paypalOptions = {
+    clientId: 'ATN4MgsCVKSXinkSTL1YqlANTikW5fXyo5C7TkyVUG7JB0DTr1G2aabkWFF9Uz6kKo61tL48cfWpomc4',
+    intent: 'capture',
+    currency: 'USD',
+  };
+
+  const buttonStyles = {
+    layout: 'vertical',
+    shape: 'rect',
+  };
+
+  const handlePaymentSuccess = (data) => {
+    if (data.status === 'COMPLETED') {
+      const newOrder = {
+        buyer,
+        products: cart,
+        payment: data,
+      };
+      addNewOrder(
+        newOrder,
+        history.push('/checkout/success'),
+      );
+    }
+  };
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  };
+
+  return (
+    <div className='Payment'>
+      <div className='Payment-content'>
+        <h3>Resument del pedido:</h3>
+        {cart.map((item) => (
+          <div className='Payment-item' key={item.title}>
+            <div className='Payment-element'>
+              <h4>{item.title}</h4>
+              <span>
+                $
+                {' '}
+                {item.price}
+              </span>
+            </div>
+          </div>
+        ))}
+        <div className='Payment-button'>
+          <PayPalButton
+            paypalOptions={paypalOptions}
+            buttonStyles={buttonStyles}
+            amount={handleSumTotal()}
+            onSuccess={(data) => handlePaymentSuccess(data)}
+            onError={(error) => console.log(error)}
+            onCancel={(data) => console.log(data)}
+          />
+        </div>
+      </div>
+      <div />
+    </div>
+  );
+};
+
+export default Payment;
